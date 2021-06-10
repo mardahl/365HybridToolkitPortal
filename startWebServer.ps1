8528a4e1317fd8b6c7e107060ea10b5bb65bd763
Michael Mardahl <iphase@iphase.dk>
2021-06-10 20:58:09 +0200

    Update startWebServer.ps1

--- startWebServer.ps1
+++ startWebServer.ps1
@@ -1,6 +1,6 @@
 #Requires -RunAsAdministrator
 Stop-Transcript
-Start-Transcript -Path "C:\Scripts\startWebServer_lastStart_log.txt" -Force 
+Start-Transcript -Path "C:\365HTK\startWebServer_lastStart_log.txt" -Force 
 Try {
     $hostname= "$($env:COMPUTERNAME).$($env:USERDNSDOMAIN)"
     Start-PoSHServer -Hostname $hostname -Port 8080 –HomeDirectory “C:\365HTK\PSWWWRoot” –LogDirectory “C:\365HTK\PSWWWLogs” -ErrorAction Stop
